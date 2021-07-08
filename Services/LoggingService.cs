7156420e31af3fa5e1c17054b82383481083e71f
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2021-07-08 09:31:04 +0200

    Added LogginService.

---
+++ Services/LoggingService.cs
@@ -0,0 +1,11 @@
+﻿using Spectre.Console;
+using System;
+
+namespace PixelSim.Services
+{
+    public class LoggingService
+    {
+        public static void WriteLine(string message) 
+            => AnsiConsole.MarkupLine($"[gray][[{DateTime.Now.ToString()}]][/] {message}");
+    }
+}
